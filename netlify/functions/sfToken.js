b9215d84008a0c3e146609ea64467443cc06cb40
Sean <180867668+uocli@users.noreply.github.com>
2025-09-15 20:10:17 -0400

    Updated

--- netlify/functions/sfToken.js
+++ netlify/functions/sfToken.js
@@ -1,25 +1,28 @@
-const fetch = require('node-fetch');
-
-exports.handler = async function(event) {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+exports.handler = async function (event) {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  const { code, code_verifier, redirect_uri, client_id } = JSON.parse(event.body);
+  const { code, code_verifier, redirect_uri, client_id } = JSON.parse(
+    event.body,
+  );
 
   const params = new URLSearchParams({
-    grant_type: 'authorization_code',
+    grant_type: "authorization_code",
     code,
     client_id,
     redirect_uri,
     code_verifier,
   });
 
-  const response = await fetch('https://login.salesforce.com/services/oauth2/token', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: params.toString(),
-  });
+  const response = await fetch(
+    "https://login.salesforce.com/services/oauth2/token",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: params.toString(),
+    },
+  );
 
   const data = await response.json();
 
